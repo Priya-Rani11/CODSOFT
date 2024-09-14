1ad4803d1f01d4309c4ee0f63ef411c8e76a5115
Priya-Rani11 <31priyarani2005@gmail.com>
2024-09-14 09:38:55 +0530

    errorfix

--- Calculator.py
+++ Calculator.py
@@ -34,53 +34,53 @@ def calculator():
         print("8. SQUARE ROOT OF A NUMBER")
         print("9. POWER OF X ^Y")
         print("10. EXIT")
-        choice=int(input("PRESS THE NUMBER :"))
+        choice=input("PRESS THE NUMBER :")
 
-        if choice in [1,2,3,4,7,9]:
+        if choice in ['1','2','3','4','7','9']:
             num1=float(input("PRESS NUMBER 1 : "))
             num2=float(input("PRESS NUMBER 2 : "))
-            if choice ==1:
+            if choice =='1':
                 
                 a=add(num1,num2)
                 print(f"SUM OF {num1} AND {num2} IS {a} .")
 
-            elif choice==2:
+            elif choice=='2':
                 
                 s=sub(num1,num2)
                 print(f"SUBTRACTION OF {num1} AND {num2} IS {s} .")
-            elif choice==3:
+            elif choice=='3':
                 
                 m=multiply(num1,num2)
                 print(f"MULTIPLICATION OF {num1} AND {num2} IS {m}")
-            elif choice==4:
+            elif choice=='4':
                 
                 d=division(num1,num2)
                 print(f"DIVISION OF {num1} AND  {num2} IS {d} .")
             
-            elif choice==7:
+            elif choice=='7':
                 print(f"TAKING {num1} AS NUMBER AND {num2} AS BASE .")
                 l=log(num1,num2)
                 print(f"LOG OF {num1} WITH {num2} AS BASE IS {l} .")
-            elif choice==9:
+            elif choice=='9':
                 print(f"CONSIDERING {num1} AS BASE AND {num2} AS EXPONENT .")
                 r=pw(num1,num2)
                 print(f"{num1} TO THE POWER{num2} IS {r} . ")
-        elif choice in [5,6,8]:
+        elif choice in ['5','6','8']:
             num=float(input("ENTER NUMBER : "))
-            if choice==6:
+            if choice=='6':
                 
                 s=qu(num)
                 print(f"CUBE OF {num} IS {s} .")
-            elif choice==5:
+            elif choice=='5':
                 
                 s=sq(num)
                 print("SQUARE OF ",num, "IS",s,".")
-            elif choice==8:
+            elif choice=='8':
                 
                 qrt=sqt(num)
                 print(f"SQUARE ROOT OF {num} IS {qrt} .")
         
-        elif choice== 10:
+        elif choice== '10':
             break
         else:
             print("INVALID CHOICE")
